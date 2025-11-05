ceb6e70d101a088e2b0490024ae8ae8ec1dc155f
kirkd2277-commits <kirkd2277@student.faytechcc.edu>
2025-11-05 11:39:59 -0500

    P1lab1
    for input for user and print.

---
+++ P1lab1 Dillon Kirk.py
@@ -0,0 +1,9 @@
+# Dillon Kirk
+# 05 Nov 2025
+# P1lab1.py
+# input and print
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name,last_name +"!","Welcome to CTI-110!")
